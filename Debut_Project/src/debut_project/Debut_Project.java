677558db0c4cb0732279da12f9ab2c48bc2f8035
Hareesh Thirunahari <S521812@NW35825.nwmissouri.edu>
2015-10-13 15:06:41 -0500

    Initial commit

--- Debut_Project/src/debut_project/Debut_Project.java
+++ Debut_Project/src/debut_project/Debut_Project.java
@@ -16,6 +16,7 @@ public class Debut_Project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
